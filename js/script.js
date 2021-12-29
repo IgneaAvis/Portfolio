5a6e7ef839578dbfe4d6fca8c3db5ea30c351650
Kostya <ko.klochkov@gmail.com>
2021-12-29 21:02:45 +0300

    final

--- js/script.js
+++ js/script.js
@@ -1,8 +1,17 @@
 $(document).ready(function(){
     $('.portfolio_slider').slick({
         autoplay: true,
-        autoplaySpeed: 3000,
-        swipe: false
+        autoplaySpeed: 4000,
+        swipe: false,
+        responsive: [
+            {
+              breakpoint: 991,
+              settings: {
+                swipe: true,
+                arrows: false,
+              }
+            }
+        ]
     });
     $(window).scroll(function () {
         if ($(this).scrollTop() > 500) {
@@ -16,5 +25,17 @@ $(document).ready(function(){
             scrollTop: 0
         }, 200);
         return false;
+    });
+    $('.burger').on('click', function(){
+        $('.burger').toggleClass('burger_active');
+        // $('body').css('position', 'fixed');
+        // $('body').css('width', '100%');
+        // $('body').css('height', '100%');
+        $('.overlay').toggleClass('overlay_active');
+    });
+    $('.overlay_menu_item').on('click', function(e) {
+        $('.overlay').removeClass('overlay_active');
+        $('.burger').removeClass('burger_active');
+        $('body').css('position', 'relative');
     });
 });
